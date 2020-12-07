843ab4df3fda29b573bd516392055ed6d7fda782
MaximeCgn <maxime.cugny@laposte.net>
2020-12-07 15:08:15 +0100

    07 12 2020 Maxime

--- PHP/VIEW/Footer.php
+++ PHP/VIEW/Footer.php
@@ -1,6 +1,12 @@
 </div>
 <div></div>
 </div>
+<div class="footer">
+<div class="texteFooter">
+            <p>©LaBoutiqueDuLin</p>
+        </div>
+</div>
+
 <footer>
         <div class="texteFooter">
             <p>©LaBoutiqueDuLin</p>
