78ea2d8d811a5844588b8562d26de4871f97abbe
MaximeCgn <maxime.cugny@laposte.net>
2020-12-07 09:26:29 +0100

    07 12 2020 Maxime

---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+
+require("./Outils.php");
+
+Parametres::init();
+
+DbConnect::init();
+
+session_start();
+
+/******Les langues******/
+/***On récupère la langue***/
+if (isset($_GET['lang']))
+{
+	$_SESSION['lang'] = $_GET['lang'];
+}
+
+/***On récupère la langue de la session/FR par défaut***/
+$lang=isset($_SESSION['lang']) ? $_SESSION['lang'] : 'FR';
+/******Fin des langues******/
+
+$routes=[
+    "default"=>["PHP/VIEW/","accueil","Accueil"],
+
+    "FormConnection"=>["PHP/VIEW/","FormConnection","Formulaire de Connexion"],
+    "FormDeconnection"=>["PHP/VIEW/","FormDeconnection","Formulaire de Deconnexion"],
+    "HtmlConnection"
+
+];
+
+if(isset($_GET["page"]))
+{
+
+	$page=$_GET["page"];
+
+	if(isset($routes[$page]))
+	{
+		AfficherPage($routes[$page]);
+	}
+	else
+	{
+		AfficherPage($routes["default"]);
+	}
+}
+else
+{
+	AfficherPage($routes["default"]);
+}
